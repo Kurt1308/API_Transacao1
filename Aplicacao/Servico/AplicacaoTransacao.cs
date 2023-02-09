61a8d53a6c0ef8f539a0ad7244c5529aceb67720
Lucas Dias <98557406+Kurt1308@users.noreply.github.com>
2023-02-09 15:48:10 -0300

    Update

--- Aplicacao/Servico/AplicacaoTransacao.cs
+++ Aplicacao/Servico/AplicacaoTransacao.cs
@@ -85,9 +85,12 @@ namespace Aplicacao.Servico
         public RespostaInsertTransacaoDto Insert(RequisicaoInsertTransacaoDto obj)
         {
             string mensagem = "";
-            bool check = VerificaLimiteCartao(obj.valor, obj.num_cartao);
-            if(check == false)
-                return _mapperTransacao.MapperToDtoInsert(HttpStatusCode.Forbidden, "Este cartão não possui limite disponível");
+            decimal check = VerificaLimiteCartao(obj.valor, obj.num_cartao);
+            if(check < 0)
+                return _mapperTransacao.MapperToDtoInsert(HttpStatusCode.Forbidden, "Este cartão não possui limite disponível.");
+            
+            //bool checkUpdateCartao = ExecutaUpdateCartao(obj.valor, obj.num_cartao);
+
             if (!mensagem.Equals(string.Empty))
                 return _mapperTransacao.MapperToDtoInsert(HttpStatusCode.UnprocessableEntity, mensagem);
             try
@@ -101,7 +104,118 @@ namespace Aplicacao.Servico
                 return _mapperTransacao.MapperToDtoInsert(HttpStatusCode.InternalServerError, erro.Message);
             }
         }
-        public static bool VerificaLimiteCartao(decimal valor, long num_cartao)
+        //public static bool ExecutaUpdateCartao(decimal valor, long num_cartao)
+        //{
+        //    using var client = new HttpClient();
+        //    using var client2 = new HttpClient();
+        //    client2.DefaultRequestHeaders.Accept.Add(
+        //    new MediaTypeWithQualityHeaderValue("application/json"));
+
+        //    client.DefaultRequestHeaders.Accept.Add(
+        //    new MediaTypeWithQualityHeaderValue("application/json"));
+
+        //    var objeto = new { id_cartao = 0, agencia_id_agencia = 0, conta_id_conta = 0, limite_saldo = 0,situacao = 0 };
+
+        //    var objeto1 = selecionarCartao(objeto, num_cartao);
+        //    var response1 = client.PostAsync("https://localhost:5014/GetCartaoPorId", objeto1).Result;
+        //    if (response1.IsSuccessStatusCode)
+        //    {
+        //        var dataObjects = response1.Content.ReadAsStringAsync().Result;
+        //        var json = JsonConvert.SerializeObject(dataObjects);
+        //        if (dataObjects.Contains("limite_saldo_disponivel"))
+        //        {
+
+        //            var valor222 = Convert.ToDecimal(GetValor(dataObjects));
+        //            int v1 = Convert.ToInt32(valor);
+        //            decimal v22 = valor222;
+
+        //            var objeto11 = selecionarCartao(v22, num_cartao);
+
+        //            var obb = new
+        //            {
+        //                id_cartao = 11,
+        //                agencia_id_agencia = 1,
+        //                conta_id_conta = 6,
+        //                limite_saldo = 500,
+        //                situacao = 1
+        //            };
+        //            var obbb = converteJson(obb);
+
+        //        var response22 = client2.PutAsync("https://localhost:5014/AtualizaCartao", obbb).Result;
+        //            if (response22.IsSuccessStatusCode)
+        //            {
+        //                var dataObjects2 = response22.Content.ReadAsStringAsync().Result;
+        //                var json2 = JsonConvert.SerializeObject(dataObjects2);
+        //                if (dataObjects.Contains("limite_saldo_disponivel"))
+        //                {
+
+        //                    var valor22 = Convert.ToDecimal(GetValor(dataObjects));
+        //                    int v12 = Convert.ToInt32(valor);
+        //                    decimal v2 = valor22;
+
+        //                    if (v1 <= v2)
+        //                        return true;
+        //                }
+        //            }
+        //            else
+        //            {
+        //                return false;
+        //            }
+
+
+        //        }
+        //    }
+
+        //        var response = client.PutAsync("https://localhost:5014/AtualizaCartao", objeto1).Result;
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        var dataObjects = response.Content.ReadAsStringAsync().Result;
+        //        var json = JsonConvert.SerializeObject(dataObjects);
+        //        if (dataObjects.Contains("limite_saldo_disponivel"))
+        //        {
+
+        //            var valor22 = Convert.ToDecimal(GetValor(dataObjects));
+        //            int v1 = Convert.ToInt32(valor);
+        //            decimal v2 = valor22;
+
+        //            if (v1 <= v2)
+        //                return true;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
+
+        //    return false;
+        //}
+        //private static StringContent converteJson(object obj)
+        //{
+        //    var json2 = JsonConvert.SerializeObject(obj);
+        //    var data2 = new StringContent(json2, Encoding.UTF8, mediaType: "application/json");
+        //    return data2;
+        //}
+        private static StringContent selecionarCartao(object obj, long num_cartao)
+        {
+            if (num_cartao == 2000000000000000)
+            {
+                var objeto = new { id_cartao = 12, agencia_id_agencia = 2, conta_id_conta = 5, limite_saldo = obj, situacao = 1 };
+                var json = JsonConvert.SerializeObject(objeto);
+                var data = new StringContent(json, Encoding.UTF8, mediaType: "application/json");
+                return data;
+            }
+            else if (num_cartao == 1000000000000000)
+            {
+                var objeto1 = new { id_cartao = 11, fkAgencia = 1, idConta = 6, limite_saldo = obj, situacao = 1 };
+                var json1 = JsonConvert.SerializeObject(objeto1);
+                var data1 = new StringContent(json1, Encoding.UTF8, mediaType: "application/json");
+                return data1;
+            }
+            var json2 = JsonConvert.SerializeObject(obj);
+            var data2 = new StringContent(json2, Encoding.UTF8, mediaType: "application/json");
+            return data2;
+        }
+        public static decimal VerificaLimiteCartao(decimal valor, long num_cartao)
         {
 
             using var client = new HttpClient();
@@ -126,16 +240,17 @@ namespace Aplicacao.Servico
                     int v1 = Convert.ToInt32(valor);
                     decimal v2 = valor22;
 
-                    if (v1 <= v2)
-                    return true;
+                    decimal v = v2 - v1;
+                    return v;
                 }
             }
             else
             {
-                return false;
+                decimal a = 0;
+                return a;
             }
-
-            return false;
+            decimal b = 0;
+            return b;
         }
         public static string GetValor(string dataObjects)
         {
